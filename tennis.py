ecdd567521ca38c005d8992cf1c6859c0f7d0214
Anuradha Weeraman <anuradha@weeraman.com>
2019-02-01 20:12:51 -0500

    Updated doc, and testing script

--- tennis.py
+++ tennis.py
@@ -1,9 +1,10 @@
 from unityagents import UnityEnvironment
-from ddpg_agent import Agent
-import torch
 import numpy as np
-from ddpg_agent import Agent
-from model import Actor
+import random
+import torch
+import matplotlib.pyplot as plt
+from ddpg_agent import Agent, ReplayBuffer
+from collections import deque
 
 env = UnityEnvironment(file_name='Tennis.app')
 
@@ -28,25 +29,40 @@ state_size = states.shape[1]
 print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
 print('The state for the first agent looks like:', states[0])
 
-#random_seed = 1
-#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#scores = np.zeros(num_agents)
+state_size = state_size * 2
+agent_1 = Agent(state_size=state_size, action_size=action_size, random_seed=1)
+agent_2 = Agent(state_size=state_size, action_size=action_size, random_seed=1)
 
-#if torch.cuda.is_available():
-#    trained_model = torch.load('checkpoint_actor.pth')
-#else:
-#    trained_model = torch.load('checkpoint_actor.pth',map_location={'cuda:0': 'cpu'})
+agent_1.actor_local.load_state_dict(torch.load('actor_1_model.pth'))
+agent_1.critic_local.load_state_dict(torch.load('critic_1_model.pth'))
 
-#agent = Agent(state_size=state_size, action_size=action_size, random_seed=random_seed)
-#agent.actor_local = Actor(state_size, action_size, random_seed).to(device)
-#agent.actor_local.load_state_dict(trained_model)
+agent_2.actor_local.load_state_dict(torch.load('actor_2_model.pth'))
+agent_2.critic_local.load_state_dict(torch.load('critic_2_model.pth'))
 
-env_info = env.reset(train_mode=False)[brain_name]     # reset the environment
-states = env_info.vector_observations                  # get the current state (for each agent)
+scores_deque = deque(maxlen=100)
+total_scores = []
+average_scores = []
 
-while True:
-    #action = agent.act(states, add_noise=False)
-    action = np.random.randn(num_agents, action_size)
-    action = np.clip(action, -1, 1)
-    env_info = env.step(action)[brain_name]
-    #states = env_info.vector_observations              # get next state (for each agent)
+for i_episode in range(1, 10):
+    env_info = env.reset(train_mode=False)[brain_name]     # reset the environment
+    states = env_info.vector_observations                  # get the current state (for each agent)
+    states = np.reshape(states, (1, state_size))
+    agent_1.reset()
+    agent_2.reset()
+
+    while True:
+        action_1 = agent_1.act(states, add_noise=False)
+        action_2 = agent_2.act(states, add_noise=False)
+        actions = np.concatenate((action_1, action_2), axis=0)
+        actions = np.reshape(actions, (1, 4))
+
+        env_info = env.step(actions)[brain_name]
+        next_states = env_info.vector_observations         # get next state (for each agent)
+        next_states = np.reshape(next_states, (1, state_size))
+        rewards = env_info.rewards                         # get reward (for each agent)
+        dones = env_info.local_done                        # see if episode finished
+
+        states = next_states                               # roll over states to next time step
+
+        if np.any(dones):                                  # exit loop if episode finished
+            break
